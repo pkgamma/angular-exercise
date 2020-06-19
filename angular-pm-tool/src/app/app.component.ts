feb954ef860bb5ac95aa38356712ae33906fcc50
pkgamma <pkgamma@gmail.com>
2020-06-19 16:04:40 +0800

    one way data binding for pics done

--- angular-pm-tool/src/app/app.component.ts
+++ angular-pm-tool/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template:  `
-  <div>
+  <div class = 'm-5'>
   <h1>{{pageTitle}}</h1>
   <pm-products></pm-products>
   </div>
