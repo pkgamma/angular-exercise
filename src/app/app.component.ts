5adc3e83d5d903c602c186716608989ee53ebc0d
pkgamma <pkgamma@gmail.com>
2020-06-22 20:19:45 +0800

    successfully set up invalid URL guarding with canActivate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template:  `
-  <div class = 'm-5'>
-    <h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   pageTitle: string = "Basic Product Management Tool";
